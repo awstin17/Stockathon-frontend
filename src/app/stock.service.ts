220dcc5815ce04e64f0b5cc16bafe77098cb1693
awstin17 <austinwiese.acw@gmail.com>
2018-12-04 12:46:39 -0800

    Changed stock serve http requests

--- src/app/stock.service.ts
+++ src/app/stock.service.ts
@@ -23,10 +23,10 @@ export class StockService {
   }
   
   addStockToFavorites(stock, id, token) {
-   return this.http.post("http://summer-austin-2018-phortonssf.c9users.io:8080/api/appUsers/5b5e9f4157434775472ccdfc/stocks?access_token=qAizKGkk3ct8P7TuHI0GY98z3LmoaIPER1KVnUmtF2YRN91HoPEg69wmK2dhf5he", stock)
+   return this.http.post("https://stockathon-backend.herokuapp.com/api/appUsers/" + id + "/stocks?access_token=" + token, stock)
   }
   
   getFavorites(id, token) {
-   return this.http.get("http://summer-austin-2018-phortonssf.c9users.io:8080/api/appUsers/" + id + "/stocks?access_token=" + token)
+   return this.http.get("https://stockathon-backend.herokuapp.com/api/appUsers/" + id + "/stocks?access_token=" + token)
   }
 }
