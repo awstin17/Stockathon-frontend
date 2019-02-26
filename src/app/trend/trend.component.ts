c90c61d96d143006961d77c14a623cbf45038bb8
awstin17 <austinwiese.acw@gmail.com>
2019-02-26 15:18:31 -0800

    Search button now knows if tickers are real or not

--- src/app/trend/trend.component.ts
+++ src/app/trend/trend.component.ts
@@ -9,7 +9,7 @@ import { map } from 'rxjs/operators';
 })
 export class TrendComponent {
 
-  constructor(private _stockservice : StockService) { }
+  constructor(private _stockservice: StockService) { }
 
   character: any;
   abbreviation: any = {
@@ -19,16 +19,16 @@ export class TrendComponent {
   closingNumbers: number[] = [];
   months: number = 12;
   i: number = 0;
-  
-  public lineChartData:Array<any> = [
-    {data: [], label: 'Series A'}
+
+  public lineChartData: Array<any> = [
+    { data: [], label: 'Series A' }
   ];
-  public lineChartLabels:Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-  public lineChartOptions:any = {
+  public lineChartLabels: Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  public lineChartOptions: any = {
     responsive: true,
     maintainAspectRatio: false
   };
-  public lineChartColors:Array<any> = [
+  public lineChartColors: Array<any> = [
     { // grey
       backgroundColor: 'rgba(2,95,247,0.2)',
       borderColor: 'rgba(79,141,244,1)',
@@ -54,81 +54,77 @@ export class TrendComponent {
       pointHoverBorderColor: 'rgba(148,159,177,0.8)'
     }
   ];
-  public lineChartLegend:boolean = true;
-  public lineChartType:string = 'line';
-  
- 
+  public lineChartLegend: boolean = true;
+  public lineChartType: string = 'line';
+
+
   updateChart() {
-    
+
     //This section loops through the object of objects that contain data for each month
-    
-    for(let date in this.data["Monthly Time Series"]) {
-      
-     //This first if statement simply retrieves the number of the month of the latest data set. 
-     //I use this to manipulate the labels array to display the months based on what the latest month is
-      
-          if(this.i === 0) {
-            this.character = (date.charAt(5) == "0") ? date.charAt(6) : date.charAt(5) + date.charAt(6);
-          }
-          
-          //This pushes the closing data number to the closing numbers array in each iteration of the loop.
-          
-          this.closingNumbers.push(this.data["Monthly Time Series"][date]["4. close"]);
-          
-          // This is adding one to my counter, and if the counter equals the number of months i want to display 
-          //(in this case 12), the loop stops
-          
-          this.i++;
-          if(this.i === this.months) {break}
-        }
-    
+
+    for (let date in this.data["Monthly Time Series"]) {
+
+      //This first if statement simply retrieves the number of the month of the latest data set. 
+      //I use this to manipulate the labels array to display the months based on what the latest month is
+
+      if (this.i === 0) {
+        this.character = (date.charAt(5) == "0") ? date.charAt(6) : date.charAt(5) + date.charAt(6);
+      }
+
+      //This pushes the closing data number to the closing numbers array in each iteration of the loop.
+
+      this.closingNumbers.push(this.data["Monthly Time Series"][date]["4. close"]);
+
+      // This is adding one to my counter, and if the counter equals the number of months i want to display 
+      //(in this case 12), the loop stops
+
+      this.i++;
+      if (this.i === this.months) { break }
+    }
+
     //This part  reverses the closing numbers data points to display right on the graph, and then assigns the array in the proper dataset
-    
+
     this.closingNumbers = this.closingNumbers.reverse();
-    this.lineChartData = [{data: this.closingNumbers, label: 'Series A'}];
-  
+    this.lineChartData = [{ data: this.closingNumbers, label: 'Series A' }];
+
     //This part rearranges the labels array so that the last month where we got data from is the last label
-  
+
     this.lineChartLabels = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    for(let k = 0; k < this.character; k++) {
-        this.lineChartLabels.push(this.lineChartLabels.shift());
+    for (let k = 0; k < this.character; k++) {
+      this.lineChartLabels.push(this.lineChartLabels.shift());
     }
   }
 
   //When the search button is clicked, this method gets the monthly data from the stock API based on the acronym in the input
   //Then, if you get a successful response, it saves that data, manipulates it to a presentable form, then updates the chart
 
-  onSearch() {
+  checkIsRealStock(searchOrAddToFavorites) {
     this._stockservice.getMonthlyData(this.abbreviation.ticker)
-    .subscribe(
-      (response: any) => {
-        
-        this.i = 0;
-        this.closingNumbers = [];
-        this.data = null;
-        this.data = response;
-        this.lineChartLabels = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-        
-        this.updateChart();
-      }
-    )
+      .subscribe(
+        (response: any) => {
+          console.log(searchOrAddToFavorites)
+          if (response["Error Message"]) { window.alert("This is not a real stock symbol. Try another") }
+          else if (searchOrAddToFavorites === "addToFavorites") { console.log(searchOrAddToFavorites); this.addStock() }
+          else if (searchOrAddToFavorites === "search") {
+            this.i = 0;
+            this.closingNumbers = [];
+            this.data = null;
+            this.data = response;
+            this.lineChartLabels = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+            this.updateChart();
+          }
+        },
+        (error) => { },
+        () => { }
+      )
   }
 
-  checkIsRealStock() {
-    this._stockservice.getMonthlyData(this.abbreviation.ticker)
-      .subscribe(
-        (response) => {if(response["Error Message"]) {window.alert("This is not a real stock symbol. Try another")}
-                       else {this.addStock()}},
-        (error) => {},
-        () => {}
-        )
-  }
-  
   addStock() {
-    this._stockservice.addStockToFavorites(this.abbreviation, window.sessionStorage.getItem('userId'),  window.sessionStorage.getItem('token'))
+    this._stockservice.addStockToFavorites(this.abbreviation, window.sessionStorage.getItem('userId'), window.sessionStorage.getItem('token'))
       .subscribe(
-        (response) => {console.log(response); alert("Successfully added to your stocks!")},
+        (response) => { console.log(response); alert("Successfully added to your stocks!") },
         (error) => console.log(error)
-        )
+      )
   }
 }
