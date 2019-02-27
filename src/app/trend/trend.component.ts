a03d7f0c80e9a94d3252dcaea8667a79422a685e
awstin17 <austinwiese.acw@gmail.com>
2019-02-26 19:37:50 -0800

    Fixed issue where chart labels were not what they should be

--- src/app/trend/trend.component.ts
+++ src/app/trend/trend.component.ts
@@ -1,6 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { StockService } from '../stock.service';
-import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-trend',
@@ -11,7 +10,6 @@ export class TrendComponent {
 
   constructor(private _stockservice: StockService) { }
 
-  character: any;
   abbreviation: any = {
     ticker: ""
   }
@@ -23,7 +21,7 @@ export class TrendComponent {
   public lineChartData: Array<any> = [
     { data: [], label: 'Series A' }
   ];
-  public lineChartLabels: Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  public lineChartLabels: Array<any> = [];
   public lineChartOptions: any = {
     responsive: true,
     maintainAspectRatio: false
@@ -57,63 +55,49 @@ export class TrendComponent {
   public lineChartLegend: boolean = true;
   public lineChartType: string = 'line';
 
+  //This OnInit creates the array of months that become the chart and table labels
+  ngOnInit() {
+    let date = new Date;
+    let monthNum = date.getMonth();
+    let arrayOfMonths = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+    for (let i = 0; i <= monthNum; i++) {
+      arrayOfMonths.push(arrayOfMonths.shift());
+    }
+     this.lineChartLabels = arrayOfMonths;
+  }
 
   updateChart() {
 
-    //This section loops through the object of objects that contain data for each month
+    //Starts by resetting indices and variables so algorithm works properly
+    this.i = 0;
+    this.closingNumbers = []
 
+    //This section loops through the object of objects that contain data for each month
     for (let date in this.data["Monthly Time Series"]) {
 
-      //This first if statement simply retrieves the number of the month of the latest data set. 
-      //I use this to manipulate the labels array to display the months based on what the latest month is
-
-      if (this.i === 0) {
-        this.character = (date.charAt(5) == "0") ? date.charAt(6) : date.charAt(5) + date.charAt(6);
-      }
-
       //This pushes the closing data number to the closing numbers array in each iteration of the loop.
-
       this.closingNumbers.push(this.data["Monthly Time Series"][date]["4. close"]);
 
       // This is adding one to my counter, and if the counter equals the number of months i want to display 
       //(in this case 12), the loop stops
-
       this.i++;
       if (this.i === this.months) { break }
     }
 
     //This part  reverses the closing numbers data points to display right on the graph, and then assigns the array in the proper dataset
-
     this.closingNumbers = this.closingNumbers.reverse();
     this.lineChartData = [{ data: this.closingNumbers, label: 'Series A' }];
-
-    //This part rearranges the labels array so that the last month where we got data from is the last label
-
-    this.lineChartLabels = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    for (let k = 0; k < this.character; k++) {
-      this.lineChartLabels.push(this.lineChartLabels.shift());
-    }
   }
 
   //When the search button is clicked, this method gets the monthly data from the stock API based on the acronym in the input
   //Then, if you get a successful response, it saves that data, manipulates it to a presentable form, then updates the chart
-
   checkIsRealStock(searchOrAddToFavorites) {
     this._stockservice.getMonthlyData(this.abbreviation.ticker)
       .subscribe(
         (response: any) => {
-          console.log(searchOrAddToFavorites)
           if (response["Error Message"]) { window.alert("This is not a real stock symbol. Try another") }
-          else if (searchOrAddToFavorites === "addToFavorites") { console.log(searchOrAddToFavorites); this.addStock() }
-          else if (searchOrAddToFavorites === "search") {
-            this.i = 0;
-            this.closingNumbers = [];
-            this.data = null;
-            this.data = response;
-            this.lineChartLabels = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-
-            this.updateChart();
-          }
+          else if (searchOrAddToFavorites === "addToFavorites") { this.addStock() }
+          else if (searchOrAddToFavorites === "search") { this.data = response; this.updateChart(); }
         },
         (error) => { },
         () => { }
